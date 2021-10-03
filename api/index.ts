2f763cf9ea37b206a8cc2ab07ca66935e4450e3e
Mikaal <matthewnaik@gmail.com>
2021-10-03 17:02:23 -0400

    tester

--- api/index.ts
+++ api/index.ts
@@ -1,10 +1,16 @@
-import express from 'express';
+const app = require('express')();
+const { v4 } = require('uuid');
 
-const app = express();
-const PORT = 3000;
-
-app.use(require('./routes/greetings'));
-
-app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}.`);
+app.get('/api', (req, res) => {
+  const path = `/api/item/${v4()}`;
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
 });
+
+app.get('/api/item/:slug', (req, res) => {
+  const { slug } = req.params;
+  res.end(`Item: ${slug}`);
+});
+
+module.exports = app;
