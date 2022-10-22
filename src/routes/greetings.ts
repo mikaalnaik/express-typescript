b0f5a59f5209ba25fa772d323fd2a7585c6a272c
Mikaal Naik <mikaalnaik@gmail.com>
2022-10-22 16:24:30 -0400

    Move code to src folder to improve structure
    Todo: get absolute paths

--- src/routes/greetings.ts
+++ src/routes/greetings.ts
@@ -1,4 +1,4 @@
-const express = require('express');
+import express from 'express';
 const router = express.Router();
 
 //Middle ware that is specific to this router
@@ -9,14 +9,15 @@ router.use(function timeLog(req, res, next) {
 
 
 // Define the home page route
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
   res.send('Howdy Folks');
 });
 
 // Define the about route
-router.get('/about', function(req, res) {
+router.get('/about', function (req, res) {
   res.send('About us');
 });
 
 
 module.exports = router;
+export default router;
